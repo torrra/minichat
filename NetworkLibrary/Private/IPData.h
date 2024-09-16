a0f04be9d0a500d861b189e2391149092612c277
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2024-09-16 12:06:35 -0400

    CREATE IPDATA STRUCT IN NETWORK LIB
    Create IPData struct to contain data about an IP address in
    NetworkLibrary/Private/IPData.h

---
+++ NetworkLibrary/Private/IPData.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+
+#include "Constants.h"
+
+namespace net
+{
+	// size: 56B, padding : 1B
+
+	// Contains information about an IP endpoint
+	struct IPData
+	{
+		std::string		m_ipString;
+
+		// WinSock addrinfo struct
+		void*			m_addrInfo;
+
+		// IPV4 address as 4B unsigned integer
+		unsigned long	m_ipAddress;
+
+		// Port number (host byte order)
+		unsigned short	m_portNumber;
+		IPVersion		m_ipVersion;
+	};
+}
