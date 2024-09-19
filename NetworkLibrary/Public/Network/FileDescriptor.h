ca8a808c8f08010413ae2e4aa5163e3a110b5e44
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2024-09-19 11:37:39 -0400

    CREATE FILE DESCRIPTOR STRUCT
    Create FileDescriptorSet structure in new file FileDescriptor.h

---
+++ NetworkLibrary/Public/Network/FileDescriptor.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#ifndef NETMAXSOCKETS
+#define NET_MAX_SOCKETS     64
+#endif
+
+#include <vector>
+
+#include "Socket.h"
+
+namespace net
+{
+    // used for select function
+    struct FileDescriptorSet
+    {
+        // same memory layout as fd_set struct
+        unsigned int           m_count;
+        Socket::Handle_t       m_sockets[NET_MAX_SOCKETS];
+    };
+
+    // used for WSAPoll function
+    struct PollData
+    {
+        // same memory layout as pollfd struct
+        Socket::Handle_t        m_socket;
+        short                   m_requestedEvents;
+        short                   m_returnedEvents;
+
+    };
+
+
+}
