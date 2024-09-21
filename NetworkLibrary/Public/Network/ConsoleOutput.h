a94504d745b063836609fdf59053eb38fe4afb03
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2024-09-21 19:01:42 -0400

    MOVE CONSOLE FUNCTIONS TO CONMSOLE OUTPUT FILES
    Move initConsole and consoleOutput in new files ConsoleOutput.h
    and ConsoleOutput.cpp in NetworkLibrary folder

---
+++ NetworkLibrary/Public/Network/ConsoleOutput.h
@@ -0,0 +1,5 @@
+namespace net
+{
+    void	consoleOutput(const char* format, ...);
+	void	initConsole();
+}
---
+++ NetworkLibrary/Source/ConsoleOutput.cpp
@@ -0,0 +1,64 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include "ConsoleOutput.h"
+#include "ErrorHandling.h"
+
+namespace net
+{
+    void consoleOutput(const char* format, ...)
+	{
+		int			dwCodepage = 0;
+		LPSTR		buffer;
+		va_list		args;
+		int			formatFlags = FORMAT_MESSAGE_ALLOCATE_BUFFER |
+			FORMAT_MESSAGE_FROM_STRING;
+
+		va_start(args, format);
+
+		int		charCount = FormatMessageA(formatFlags, (LPSTR)format, 0,
+			dwCodepage,
+			(LPSTR)&buffer, 0, &args);
+
+		if (charCount)
+		{
+			void* handle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+			if (!WriteConsoleA(handle, buffer, charCount, nullptr, 0))
+			{
+				int		error = GetLastError();
+
+				OutputDebugStringA("WriteConsole format failure\n");
+				error = error;
+
+			}
+
+			LocalFree(buffer);
+
+		}
+		else
+		{
+			int		error = GetLastError();
+
+			OutputDebugStringA("FormatMessage format failure\n");
+			DebugBreak();
+			error = error;
+		}
+
+
+	}
+
+	void initConsole()
+	{
+		int		dwCodePage;
+		int		localeFlags = LOCALE_IDEFAULTANSICODEPAGE | LOCALE_RETURN_NUMBER;
+		int		success = GetLocaleInfoEx(L"en-US", localeFlags,
+			(LPWSTR)&dwCodePage,
+			sizeof dwCodePage / sizeof WCHAR);
+
+		if (success)
+			SetConsoleOutputCP(dwCodePage);
+		else
+			reportWindowsError("initConsole", GetLastError());
+	}
+}
