2318ec0169d99840cc90971de3bef96d1d573eb5
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2024-09-21 22:32:35 -0400

    USE CLIENT APP INSTEAD OF SOCKET IN CLIENT MAIN FUNCTION
    Use a ClientApp object instead of a socket object in main.cpp

--- MiniChatClient/Source/main.cpp
+++ MiniChatClient/Source/main.cpp
@@ -1,7 +1,7 @@
 #include <Network/Network.h>
 #include <Network/Socket.h>
 
-#include <WinSock2.h>
+#include "ClientApp.h"
 
 #include <iostream>
 
@@ -9,29 +9,16 @@ int main(void)
 {
 	net::startup();
 
-    net::Socket client;
-
-    //client.create("10.207.0.59", "8888", false);
-    //
-    //client.create("172.27.192.1", "8888", false);
-    client.createClient("192.168.2.53", "8888");
-    //client.createClient("10.207.0.93", "3983");
-
-    //client.createClient("10.207.0.59", "8888");
-
-    char buffer[6];
+    client::ClientApp   client;
+    client.welcomeMessage();
 
     bool debug = true;
 
-    int bytes = client.receive(buffer, 6);
-   //int bytes = client.send(236, "Testing...\0", 12);
 
-  /*  if (bytes == -1)
-        std::cout << WSAGetLastError() << "\n";*/
-
-    std::cout << "bytes received: " << bytes << "\nbuffer : " << buffer << std::endl;
-
-    while (debug);
+    while (debug)
+    {
+        client.update();
+    }
 
     net::cleanup();
 
