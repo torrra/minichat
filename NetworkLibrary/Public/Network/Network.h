5c130983d5b0f88767ffdaeb7423780d63a0bd43
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2024-09-21 22:54:03 -0400

    REMOVE UNUSED NETWORK FUNCTION DECLARATIONS
    Remove network() and checkError() functions from Network.h

--- NetworkLibrary/Public/Network/Network.h
+++ NetworkLibrary/Public/Network/Network.h
@@ -1,15 +1,7 @@
 # pragma once
 
-#include <functional>
-
-// TODO: remove
-void network(void);
-
 namespace net
 {
     int     startup(void);
     int     cleanup(void);
-
-    template <typename TRetrurnType>
-    inline int     checkError(std::function<TRetrurnType()>);
 }
