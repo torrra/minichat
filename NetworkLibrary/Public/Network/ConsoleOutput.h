aa2c70ad3ca9f49f137c300560b13c20a0597ce0
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2024-09-23 15:20:39 -0400

    MOVE UNICODE MACROS TO CONSOLE OUTPUT FILES
    Move unicode macro definitions to ConsoleOutput.h

--- NetworkLibrary/Public/Network/ConsoleOutput.h
+++ NetworkLibrary/Public/Network/ConsoleOutput.h
@@ -1,5 +1,20 @@
+#pragma once
+
+#ifdef UNICODE
+#undef UNICODE
+#undef _UNICODE
+#endif
+
+#if 1
+#define UNICODE
+#define _UNICODE
+#endif
+
 namespace net
 {
+	// Format string and print to console
     void	consoleOutput(const char* format, ...);
+
+	// Set console codepage
 	void	initConsole();
 }
