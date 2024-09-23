8c9255fb9e359cd016e5c9e21f074312bc643486
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2024-09-23 13:00:25 -0400

    ADD MEMORY LEAK CHECKS TO CLIENT PROJECT MAIN
    Add printMemory and clientMain function to check memory states
    before and after running main client function in main.cpp

--- MiniChatClient/Source/main.cpp
+++ MiniChatClient/Source/main.cpp
@@ -3,24 +3,61 @@
 
 #include "ClientApp.h"
 
-#include <iostream>
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+static int clientMain(void)
+{
+	int		error = net::startup();
+    bool	running = true;
+
+	if (error)
+		return error;
+
+    client::ClientApp   client;
+
+	// ask user to enter IP
+    client.welcomeMessage();
+
+	// run until
+    while (running)
+        client.update(running);
+
+	return net::cleanup();
+}
+
+static void printMemory(_CrtMemState* start, _CrtMemState* end)
+{
+	// difference between start end end state
+	_CrtMemState	diff;
+
+	// print memory information
+	if (_CrtMemDifference(&diff, start, end))
+	{
+		OutputDebugString(TEXT(" ------ _CrtMemDumpStatistics ------ "));
+		_CrtMemDumpStatistics(&diff);
+
+		OutputDebugString(TEXT(" ------ _CrtMemDumpAllObjectsSince ------ "));
+		_CrtMemDumpAllObjectsSince(start);
+
+		OutputDebugString(TEXT(" ------ _CrtDumpMemoryLeaks ------"));
+		_CrtDumpMemoryLeaks();
+	}
+}
 
 int main(void)
 {
-	net::startup();
+	// Memory states to look for memory leaks
+	_CrtMemState	start;
+	_CrtMemState	end;
 
-    client::ClientApp   client;
-    client.welcomeMessage();
+	_CrtMemCheckpoint(&start);
 
-    bool debug = true;
+	// Main application function
+	int		returnValue = clientMain();
 
+	_CrtMemCheckpoint(&end);
+	printMemory(&start, &end);
 
-    while (debug)
-    {
-        client.update();
-    }
-
-    net::cleanup();
-
-	return 0;
+	return returnValue;
 }
