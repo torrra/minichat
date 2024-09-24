5204d1fe5b6a6468f89fb0c73a84f88f91cf5169
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2024-09-24 11:42:37 -0400

    SHUT DOWN ALL CLIENTS WHEN COMMAND IS RECEIVED
    Send shutdown string to all clients when server received /system
    shutdown command

--- MiniChatServer/Source/MessageHandling.cpp
+++ MiniChatServer/Source/MessageHandling.cpp
@@ -2,6 +2,11 @@
 
 #include "MessageHandling.h"
 
+#ifndef SHUTDOWN_MSG
+#define SHUTDOWN_MSG "jrII0JB0y6"
+#endif // !NET_SHUTDOWN_MSG
+
+
 namespace server
 {
 	void processMessages(net::Server& server, bool& running)
@@ -14,10 +19,11 @@ namespace server
 
             if (message.find("/system shutdown") != std::string::npos)
             {
+                size_t      msgLen = strnlen_s(SHUTDOWN_MSG, NET_MAX_PACKET_SIZE);
+
                 running = false;
                 net::consoleOutput("Shutting down server...\n");
-                server.terminate();
-                return;
+                server.createPacket(SHUTDOWN_MSG, msgLen);
             }
 
             net::consoleOutput(message.c_str());
