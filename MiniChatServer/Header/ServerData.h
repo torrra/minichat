8c97b8075f09d8167cdfe9f5c68c6959ca01751a
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2024-09-27 12:07:19 -0400

    MOVE CHANNEL SUPPORT TO SERVER APP CLASS
    Create ServerApp class to replace functions in MessageHandling.h

--- MiniChatServer/Header/ServerData.h
+++ MiniChatServer/Header/ServerData.h
@@ -1,16 +1,87 @@
 #pragma once
 
 #include <Network/Server.h>
-
 #include <unordered_map>
+
+#include "Chatroom.h"
 
 namespace server
 {
     struct ServerData
 	{
 		using ClientMap = std::unordered_map<net::Socket::Handle_t, std::string>;
+		using RoomMap = std::unordered_map<net::Socket::Handle_t, size_t>;
 
-		ClientMap		m_clientNames;
-		net::Server		m_server;
+		net::Server					m_server;
+		ClientMap					m_clientNames;
+		RoomMap						m_clientRooms;
+		std::vector<Chatroom>		m_rooms;
+	};
+
+	class ServerApp
+	{
+	private:
+
+		struct Message
+		{
+			std::string				m_text;
+			net::Socket::Handle_t	m_sender;
+		};
+
+		using ClientMap = std::unordered_map<net::Socket::Handle_t, std::string>;
+		using RoomMap = std::unordered_map<net::Socket::Handle_t, size_t>;
+
+	public:
+
+		ServerApp(void) = default;
+		~ServerApp(void) = default;
+
+		// Receive and read all messages
+		void			updateServer(void);
+		void			processMessages();
+
+		// Accessors
+
+		bool			isRunning()				const;
+		net::Server&	server();
+
+	private:
+
+		// Command handling
+
+		bool	checkCommands(void);
+		void	nicknameCommand(void);
+		void	shutdownCommand(void);
+
+		// Username processing
+		void    displayCurrentUsers(void);
+
+		// Chatroom creation / assignment
+
+		bool	createChatroom(void);
+		void	moveToRoom(void);
+
+		void    removeFromRoom(Chatroom& room, net::Socket toRemove,
+							   bool disconnected = false);
+
+		void    assignRoom(const net::Socket::Handle_t client);
+
+		// Send message to correct room
+
+		void    sendReceivedMessage(void);
+		void	formatReceivedMessage(void);
+		void	sendToServerRoom(Chatroom& room, const std::string& message);
+
+
+		// Utility
+
+		void	removeNewlines(std::string& string);
+
+		net::Server					m_server;
+		ClientMap					m_clientNames;
+		RoomMap						m_clientRooms;
+		Message						m_currentMessage;
+		std::vector<Chatroom>		m_rooms;
+		bool						m_running = true;
 	};
 }
---
+++ MiniChatServer/Source/ServerApp.cpp
@@ -0,0 +1,334 @@
+#include "ServerData.h"
+
+#include <Network/ConsoleOutput.h>
+
+#include "MessageHandling.h"
+
+#ifndef SHUTDOWN_MESSAGE
+#define SHUTDOWN_MESSAGE    "jrII0JB0y6"
+#endif // !SHUTDOWN_MESSAGE
+
+
+namespace server
+{
+    void ServerApp::updateServer(void)
+    {
+        m_server.serverUpdate();
+
+        const net::Server::SocketArray&     invalidClients = m_server.getInvalid();
+
+        for (const net::Socket& disconnected : invalidClients)
+        {
+            if (m_rooms.empty())
+                break;
+
+            size_t          roomID = m_clientRooms[disconnected.getHandle()];
+            Chatroom&       room = m_rooms[roomID];
+            std::string     leaveMsg = m_clientNames[disconnected.getHandle()];
+
+            leaveMsg += " has left\r\n\r\n";
+            removeFromRoom(room, disconnected, true);
+            sendToServerRoom(room, leaveMsg);
+        }
+        processMessages();
+    }
+
+    void ServerApp::processMessages(void)
+    {
+        std::vector<net::Packet>    packets = m_server.receiveAllPackets();
+
+        for (const net::Packet& received : packets)
+        {
+            m_currentMessage =
+            {
+                .m_text = net::Packet::unpackMessage(received),
+                .m_sender = received.getSender().getHandle()
+            };
+
+            assignRoom(m_currentMessage.m_sender);
+
+            // send message to other clients if it is not a command
+            if (!checkCommands())
+                formatReceivedMessage();
+        }
+        m_server.sendAllPackets();
+    }
+
+    bool ServerApp::isRunning() const
+    {
+        return m_running;
+    }
+
+    net::Server& ServerApp::server()
+    {
+        return m_server;
+    }
+
+    bool ServerApp::checkCommands(void)
+    {
+        if (m_currentMessage.m_text[0] != '/')
+            return false;
+
+        // check command type
+        switch (m_currentMessage.m_text[1])
+        {
+
+            // system commands
+        case 's':
+            shutdownCommand();
+            return true;
+
+            // commands related to user data
+        case 'n':
+            nicknameCommand();
+            return true;
+
+        case 'c':
+            createChatroom();
+            return true;
+
+        case 'm':
+            moveToRoom();
+            return true;
+
+        default: break;
+        }
+
+        return false;
+    }
+
+    void ServerApp::shutdownCommand(void)
+    {
+        if (m_currentMessage.m_text.find("shutdown") != std::string::npos)
+        {
+            size_t      len = strnlen_s(SHUTDOWN_MESSAGE, NET_MAX_PACKET_SIZE);
+
+            m_running = false;
+            net::consoleOutput("Shutting down server...\n");
+
+            // send shutdown message to all clients
+           m_server.createPacket(SHUTDOWN_MESSAGE, len);
+        }
+    }
+
+    void ServerApp::nicknameCommand(void)
+    {
+        size_t      pos = m_currentMessage.m_text.find(' ');
+
+        // invalid command or error reading "register" string
+        if (pos == std::string::npos)
+            return;
+
+        bool            isNew = !m_clientNames.contains(m_currentMessage.m_sender);
+        std::string     username = m_currentMessage.m_text.substr(pos + 1ull);
+
+        if (!isNew)
+        {
+            // remove newline characters that may have been added by client
+            removeNewlines(username);
+
+            size_t           roomID = m_clientRooms[m_currentMessage.m_sender];
+            Chatroom&        userRoom = m_rooms[roomID];
+            std::string      renameMsg = m_clientNames[m_currentMessage.m_sender] +
+                                        " has been renamed to " + username +
+                                        "\r\n\r\n";
+
+            // send name update to all users
+            sendToServerRoom(userRoom, renameMsg);
+            m_clientNames[m_currentMessage.m_sender] = username;
+        }
+        else
+        {
+           // list other users connected if client is new
+           m_clientNames[m_currentMessage.m_sender] = username;
+           displayCurrentUsers();
+        }
+    }
+
+    void ServerApp::displayCurrentUsers(void)
+    {
+        net::Socket             severSocket = m_server.getSocket();
+        std::string             username = m_clientNames[m_currentMessage.m_sender];
+        std::string             connectedUsers = "Other users connected: ";
+        net::Socket::Handle_t   senderHandle = m_currentMessage.m_sender;
+        Chatroom&               userRoom = m_rooms[m_clientRooms[senderHandle]];
+
+        // add all clients to current users list
+        for (auto& client :m_clientNames)
+        {
+            if (client.first == senderHandle ||
+                m_clientRooms[senderHandle] != m_clientRooms[client.first])
+            {
+                continue;
+            }
+
+            connectedUsers += client.second;
+            connectedUsers += ", ";
+        }
+        connectedUsers += "\r\n";
+
+        // send message to new user only
+        severSocket.sendTo(m_currentMessage.m_sender,
+                           connectedUsers.c_str(),
+                           (int)connectedUsers.size());
+
+        // tell other clients that a new user has joined
+       sendToServerRoom(userRoom, username + " has joined.\r\n\r\n");
+    }
+
+    void ServerApp::removeNewlines(std::string& str)
+    {
+        for (size_t index = str.size() - 1ull; index > 0ull; --index)
+        {
+            // delete newline characters and leave at least
+            // 1 character in string
+            switch (str[index])
+            {
+            case '\n':
+                str.pop_back();
+                continue;
+
+            case '\r':
+                str.pop_back();
+                continue;
+
+            default: return;
+            }
+        }
+    }
+
+    bool ServerApp::createChatroom(void)
+    {
+        size_t          pos = m_currentMessage.m_text.find('#');
+        std::string     roomName = m_currentMessage.m_text.substr(pos);
+        size_t          currentIndex = m_clientRooms[m_currentMessage.m_sender];
+        Chatroom&       currentRoom = m_rooms[currentIndex];
+
+        if (roomName.empty())
+            return false;
+
+        removeNewlines(roomName);
+        removeFromRoom(currentRoom, m_currentMessage.m_sender);
+
+       m_clientRooms[m_currentMessage.m_sender] = m_rooms.size();
+       m_rooms.emplace_back(roomName, m_currentMessage.m_sender);
+
+        std::string     roomMsg = "Room created successfully\r\n";
+        net::Socket     serverSock = m_server.getSocket();
+
+        serverSock.sendTo(m_currentMessage.m_sender, roomMsg.c_str(), (int)roomMsg.size());
+        net::consoleOutput(roomMsg.c_str());
+        return true;
+    }
+
+    void ServerApp::moveToRoom(void)
+    {
+        std::string     m_currentMessageCopy = m_currentMessage.m_text;
+        size_t          roomIndex = 0;
+        size_t          namePos = m_currentMessageCopy.find('#');
+
+        if (namePos == std::string::npos)
+            return;
+
+        removeNewlines(m_currentMessageCopy);
+        m_currentMessageCopy = m_currentMessageCopy.substr(namePos);
+
+        for (Chatroom& room :m_rooms)
+        {
+            if (room.m_roomName.compare(m_currentMessageCopy) == 0)
+            {
+                room.m_users.push_back(m_currentMessage.m_sender);
+
+                std::string     message = m_clientNames[m_currentMessage.m_sender];
+                size_t          currentIndex = m_clientRooms[m_currentMessage.m_sender];
+                Chatroom&       currentRoom = m_rooms[currentIndex];
+
+               removeFromRoom(currentRoom, m_currentMessage.m_sender);
+               m_clientRooms[m_currentMessage.m_sender] = roomIndex;
+
+               sendToServerRoom(room, message + " has joined.\r\n\r\n");
+               sendToServerRoom(currentRoom, message + " has left.\r\n\r\n");
+               displayCurrentUsers();
+               break;
+            }
+            ++roomIndex;
+        }
+    }
+
+    void ServerApp::sendReceivedMessage(void)
+    {
+        size_t          roomID = m_clientRooms[m_currentMessage.m_sender];
+        Chatroom&       room = m_rooms[roomID];
+        net::Socket     serverSocket = m_server.getSocket();
+
+        for (net::Socket& recipient : room.m_users)
+        {
+            if (recipient == m_currentMessage.m_sender)
+                continue;
+
+            serverSocket.sendTo(recipient, m_currentMessage.m_text.c_str(), (int)m_currentMessage.m_text.size());
+        }
+    }
+
+    void ServerApp::formatReceivedMessage(void)
+    {
+        std::string     toDisplay;
+        size_t          roomIndex = m_clientRooms[m_currentMessage.m_sender];
+
+        toDisplay = m_rooms[roomIndex].m_roomName;
+
+        // add username before message
+        toDisplay += " | " +m_clientNames[m_currentMessage.m_sender];
+        toDisplay += " > " + m_currentMessage.m_text;
+        net::consoleOutput(toDisplay.c_str());
+
+        m_currentMessage.m_text = toDisplay;
+        sendReceivedMessage();
+    }
+
+    void ServerApp::sendToServerRoom(Chatroom& room, const std::string& message)
+    {
+        for (const net::Socket& client : room.m_users)
+        {
+            m_server.getSocket().sendTo(client, message.c_str(),
+                                        (int) message.size());
+        }
+    }
+
+    void ServerApp::removeFromRoom(Chatroom& room, net::Socket toRemove,
+                                   bool disconnected)
+    {
+        for (auto iterator = room.m_users.begin();
+            iterator != room.m_users.end(); ++iterator)
+        {
+            if (*iterator != toRemove)
+                continue;
+
+            if(disconnected)
+            {
+                m_clientRooms.erase(toRemove.getHandle());
+                m_clientNames.erase(toRemove.getHandle());
+            }
+
+            room.m_users.erase(iterator);
+            break;
+
+        }
+    }
+
+    void ServerApp::assignRoom(const net::Socket::Handle_t client)
+    {
+        // Create default room if none already exists
+        if (m_rooms.empty())
+        {
+           m_rooms.emplace_back("#general", client);
+           m_clientRooms[client] = 0;
+        }
+        if (!m_clientRooms.contains(client))
+        {
+           m_clientRooms[client] = 0;
+           m_rooms[0].m_users.push_back(client);
+        }
+    }
+
+}
